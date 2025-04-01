7e1e03455baa3fb3a32b7d21d3cce0093b4da5d7
wajahat ali <wajahatfaryadali@gmail.com>
2025-04-01 21:52:59 +0500

    shadcn installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,9 @@ export default function Home() {
     return (
         <ClerkProvider>
             <SignedOut>
-                <SignInButton mode="modal" />
+                <SignInButton mode="modal">
+                    <button className="p-1 font-bold uppercase bg-gray-500 text-white rounded-sm my-1s">sign in</button>
+                </SignInButton>
                 <div>|</div>
                 <SignUpButton mode="modal" />
             </SignedOut>
